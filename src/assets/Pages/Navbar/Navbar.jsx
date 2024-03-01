a9835ec77f55ee2a3f197b6f62402334aed4e431
amraghbar <amrtaghbar@gmail.com>
2024-03-01 02:07:59 +0200

    swiper27t

--- src/assets/Pages/Navbar/Navbar.jsx
+++ src/assets/Pages/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
       <li className="nav-item">
-          <NavLink className="nav-link text-light" to='/home'>Home</NavLink>
+          <NavLink className="nav-link text-light" to='/Home'>Shop </NavLink>
         </li>
         <li className="nav-item">
           <NavLink className="nav-link text-light" to='/Categories'>Categories</NavLink>
