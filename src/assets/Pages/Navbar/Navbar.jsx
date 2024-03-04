ac29685cddca004cd5e5b6cfd257fd328b925e8b
amraghbar <amrtaghbar@gmail.com>
2024-03-04 02:12:41 +0200

    sddf

--- src/assets/Pages/Navbar/Navbar.jsx
+++ src/assets/Pages/Navbar/Navbar.jsx
@@ -44,7 +44,7 @@ function Navbar() {
             <NavLink className="nav-link dropdown-toggle" to role="button" data-bs-toggle="dropdown" aria-expanded="false">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle text-light " viewBox="0 0 16 16">
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                <path fillrule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
               </svg>
             </NavLink>
             <ul className="dropdown-menu">
@@ -58,7 +58,7 @@ function Navbar() {
           </li>
         </button>
         <NavLink className="nav-item bg-danger" to="./Carshop">
-          <button className={`btn" btn-outline-success nav-item bg-danger ${style.btncl}`} type="submit" onclick="{handleShow}">
+          <button className={`btn" btn-outline-success nav-item bg-danger ${style.btncl}`} type="submit" onClick={handleShow}>
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="30" fill="currentColor" className="bi bi-cart-dash text-light" viewBox="0 0 16 16">
               <path d="M6.5 7a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1z" />
               <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
@@ -66,12 +66,12 @@ function Navbar() {
           </button>
         </NavLink>
         <Offcanvas show={show} onHide={handleClose}>
-  <Offcanvas.header closeButton>
-    <Offcanvas.title>Shopping cart</Offcanvas.title>
-  </Offcanvas.header>
-  <Offcanvas.body>
+  <Offcanvas.Header closeButton>
+    <Offcanvas.Title>Shopping cart</Offcanvas.Title>
+  </Offcanvas.Header>
+  <Offcanvas.Body>
     حلوه ولا مش حلوه 
-  </Offcanvas.body>
+  </Offcanvas.Body>
 </Offcanvas>
 
       </form>
