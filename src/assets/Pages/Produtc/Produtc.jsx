90e1cd3aa8485cbd0066c3a3b6d618e369efdaf5
amraghbar <amrtaghbar@gmail.com>
2024-03-06 01:00:55 +0200

    usepeeee

--- src/assets/Pages/Produtc/Produtc.jsx
+++ src/assets/Pages/Produtc/Produtc.jsx
@@ -31,37 +31,36 @@ function Produtc() {
       ) : (
         <>
           <h4>Product ID: {id}</h4>
-          {product.map((xx, index) => (
-            <div key={index} className="row row-cols-1 row-cols-md-2 g-4">
-              <div className="col">
-                <div className=" mb-3">
-                  <div className="row g-0">
-                    <div className="col-md-4">
-                      <img
-                        src={xx.mainImage.secure_url}
-                        style={{ width: "100%", height: "81%" }}
-                        className="img-fluid rounded-start"
-                        alt={xx.name}
-                      />
-                    </div>
-                    <div className="col-md-8">
-                      <div className="card-body">
-                        <h5 className="card-title">{xx.name}</h5>
-                        <p className="card-text">{xx.description}</p>
-                        <p className="card-text">{xx.slug}</p>
-                        <p className="card-text">{xx.price}$</p>
-                        <p className="card-text">{xx.status}</p>
-                      </div>
+          <div className="row row-cols-1 row-cols-md-2 g-4">
+            <div className="col">
+              <div className=" mb-3">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img
+                      src={product.mainImage.secure_url}
+                      style={{ width: "100%", height: "81%" }}
+                      className="img-fluid rounded-start"
+                      alt={product.name}
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">{product.name}</h5>
+                      <p className="card-text">{product.description}</p>
+                      <p className="card-text">{product.slug}</p>
+                      <p className="card-text">{product.price}$</p>
+                      <p className="card-text">{product.status}</p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          ))}
+          </div>
         </>
       )}
     </>
   );
+  
 }
 
 export default Produtc;
