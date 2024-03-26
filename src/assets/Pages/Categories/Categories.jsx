db58079fe343d0760b78e617063ee8e4acadc92d
amraghbar <amrtaghbar@gmail.com>
2024-03-27 01:43:33 +0200

    finaly

--- src/assets/Pages/Categories/Categories.jsx
+++ src/assets/Pages/Categories/Categories.jsx
@@ -1,47 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import { Link } from "react-router-dom";
 
 function Categories() {
   const [categories, setCategories] = useState([]);
 
   const getData = async () => {
-    const { data } = await axios.get(`${import.meta.env.VITE_API}/categories/active?page=1&limit=9`,);
-  
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_API}/categories/active?page=1&limit=9`
+    );
+
     setCategories(data.categories);
   };
 
-
-
   useEffect(() => {
     getData();
-    
   }, []);
 
-  
   return (
     <>
-      <div style={{ paddingTop: '50px' }}>
-      <h2 style={{ backgroundColor: 'red', color: 'white', padding: '10px', borderRadius: '5px', marginTop: '20px',  textAlign: 'center' }}>Categories</h2>
-    </div>
-     <Swiper
-       spaceBetween={40}
-       slidesPerView={5.5} 
-       pagination={{ clickable: true }}
-      style={{ margin: '10px' }}
-    >
-      {categories.map((x) => (
-        <SwiperSlide key={x.id}>
-          <div>
-            <h2>{x.name}</h2>
-            <img src={x.image.secure_url} alt="" />
-            <Link to={`/Categories/${x._id}`}> Details</Link> 
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+      <div style={{ paddingTop: "50px" }}>
+        <h2
+          style={{
+            backgroundColor: "red",
+            color: "white",
+            padding: "10px",
+            borderRadius: "5px",
+            marginTop: "20px",
+            textAlign: "center",
+          }}
+        >
+          Categories
+        </h2>
+      </div>
+      <Swiper
+        spaceBetween={40}
+        slidesPerView={3.6}
+        pagination={{ clickable: true }}
+        style={{ margin: "10px" }}
+      >
+        {categories.map((x) => (
+          <SwiperSlide key={x.id}>
+            <div className="row justify-content-center align-items-center">
+              <h2>{x.name}</h2>
+              <img
+                src={x.image.secure_url}
+                alt=""
+                style={{ width: "200px", height: "200px" }}
+              />
+              <Link
+                className="btn btn-outline-dark text-decoration-none text-dark"
+                to={`/Categories/${x._id}`}
+                style={{
+                  backgroundColor: "red",
+                  borderColor: "blue",
+                  color: "white",
+                }}
+              >
+                Details
+              </Link>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   );
 }
